0fe9480340f65b22c47d90c6fa31cc249bea39a9
NgChris415 <132420552+NgChris415@users.noreply.github.com>
2023-06-22 12:57:58 -0700

    Added sql/erd files for assignment

---
+++ mysql/queries/Ng_Chris_My_Workbench_Setup.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM names;
+
+INSERT INTO names(id, name, created_at, updated_at)
+VALUES (1, "Chris Ng", Now(), Now());
+
+INSERT INTO names(id, name, created_at, updated_at)
+VALUES (2, "Graciela Tejokusumo", Now(), Now()), (3, "Kya Tedjokusumo", Now(), Now());
